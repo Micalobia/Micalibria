effa95cbb7e930765b9fc993ea1186e365c58db2
Micah <micalobia2@gmail.com>
2021-12-13 22:39:31 -0800

    Complete reset basically, changing the entire scope of this library

--- src/main/java/dev/micalobia/micalibria/MicalibriaMod.java
+++ src/main/java/dev/micalobia/micalibria/MicalibriaMod.java
@@ -1,14 +1,14 @@
 package dev.micalobia.micalibria;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MicalibriaMod implements ModInitializer {
+	public static final String MODID = "micalibria";
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
 	}
 }
