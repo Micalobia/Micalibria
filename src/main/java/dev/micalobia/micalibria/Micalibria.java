7da999659a83bae0a8d13989f3c034fd8dc8f724
Micah <micalobia2@gmail.com>
2021-12-14 01:00:35 -0800

    Removed entrypoint

--- src/main/java/dev/micalobia/micalibria/Micalibria.java
+++ src/main/java/dev/micalobia/micalibria/Micalibria.java
@@ -1,14 +1,14 @@
 package dev.micalobia.micalibria;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class MicalibriaMod implements ModInitializer {
+public class Micalibria {
 	public static final String MODID = "micalibria";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
-	@Override
-	public void onInitialize() {
+	public static Identifier id(String name) {
+		return new Identifier(MODID, name);
 	}
 }
