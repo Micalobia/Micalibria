8147bd12bc292270c577e4327bed70ce0988e9d2
Micah <micalobia2@gmail.com>
2022-01-20 05:23:59 -0800

    Added VersionedMixinsPlugin

---
+++ src/main/java/dev/micalobia/micalibria/mixin/VersionedMixinsPlugin.java
@@ -0,0 +1,85 @@
+package dev.micalobia.micalibria.mixin;
+
+import net.minecraft.SharedConstants;
+import org.jetbrains.annotations.Nullable;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * Will check the full classname of a mixin and only allow it if it passes the version check
+ * <br/>
+ * <code>.1_16_3__. => After 1.16.3 (inclusive)<br/>
+ * .__1_15_2. => Before 1.15.2 (inclusive)<br/>
+ * .1_13__1_18_1. => Between 1.13.x and 1.18.1 (inclusive)</code>
+ */
+public class VersionedMixinsPlugin implements IMixinConfigPlugin {
+	private static final Pattern versionPattern =
+			Pattern.compile("^(\\d+)(?:\\.(\\d+)(?:\\.(\\d+))?)?$");
+	private static final Pattern versionRangePattern =
+			Pattern.compile("\\.(?!__\\.)(?<min>\\d+(?:(?:_\\d+)?_\\d+)?)?__(?<max>\\d+(?:(?:_\\d+)?_\\d+)?)?\\.");
+
+	private static boolean between_version(@Nullable String minimum, @Nullable String maximum) {
+		//noinspection deprecation
+		String version = SharedConstants.VERSION_NAME;
+		var matcher = versionPattern.matcher(version);
+		if(!matcher.matches()) return false;
+		var minMatcher = versionPattern.matcher(Objects.requireNonNullElse(minimum, "0.0.0"));
+		if(!minMatcher.matches()) throw new IllegalArgumentException("minimum was not valid!");
+		var maxMatcher = versionPattern.matcher(Objects.requireNonNullElse(maximum, "999.999.999"));
+		if(!maxMatcher.matches()) throw new IllegalArgumentException("maximum was not valid!");
+		for(int i = 0; i < 3; ++i) {
+			if(matcher.groupCount() >= i) return true;
+			var val = Integer.parseInt(matcher.group(i));
+			var min = minMatcher.groupCount() >= i ? 0 : Integer.parseInt(minMatcher.group(i));
+			if(val < min) return false;
+			var max = maxMatcher.groupCount() >= i ? 999 : Integer.parseInt(maxMatcher.group(i));
+			if(val > max) return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void onLoad(String mixinPackage) {
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		var matcher = versionRangePattern.matcher(mixinClassName);
+		while(matcher.find()) {
+			String min = matcher.group("min");
+			if(min != null) min = min.replace('_', '.');
+			String max = matcher.group("max");
+			if(max != null) max = max.replace('_', '.');
+			if(!between_version(min, max)) return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+}
