554d8847b4af428781a1d27ffa3c589eef73db37
Micah <micalobia2@gmail.com>
2022-01-22 07:20:21 -0800

    Promoted a Type to Class<T>

--- src/main/java/dev/micalobia/micalibria/util/nbt/NbtDeserializationContext.java
+++ src/main/java/dev/micalobia/micalibria/util/nbt/NbtDeserializationContext.java
@@ -2,8 +2,6 @@ package dev.micalobia.micalibria.util.nbt;
 
 import net.minecraft.nbt.NbtElement;
 
-import java.lang.reflect.Type;
-
 public interface NbtDeserializationContext {
-	<T> T deserialize(NbtElement element, Type klass);
+	<T> T deserialize(NbtElement element, Class<T> klass);
 }
